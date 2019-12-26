93c415763f789d7f9e9359133d7e02e71e1a45a1
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-12-26 01:57:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = function(record) {
+  let win = record.find(record => record.result === "w");
+  if (win) {
+    return win.year;
+  }
+}
+
